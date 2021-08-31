742a5b4b7918fb861010e1cf1e57f1106ee2eba0
MatheusTurra <matheusfr.318@gmail.com>
2021-08-31 11:53:16 -0300

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'password');
+  define('DB_NAME', 'Framework');
+
+  define('APPROOT', dirname(dirname(__FILE__)));
+  define('URLROOT', 'http://localhost/frameworkApp');
+  define('SITENAME', 'Framework App');
